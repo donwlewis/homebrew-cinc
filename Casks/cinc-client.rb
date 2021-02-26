b20b17eb01a119fe0d7b20f001713d9a436d6b1e
Don Lewis <don.wayne.lewis@gmail.com>
2021-02-26 09:18:08 -0700

    Remove arch.

--- Casks/cinc-client.rb
+++ Casks/cinc-client.rb
@@ -1,7 +1,6 @@
 cask "cinc-client" do
   version "16.10.17"
   sha256 "b41b31098eaefdefc0bcdd26f6c7da1b1d9314c2cf0b9a68ef23895c67bcb76c"
-  arch "x86_64"
 
   url "http://downloads.cinc.sh/files/stable/cinc/#{version}/mac_os_x/10.14/cinc-#{version}-1.x86_64.dmg"
   appcast ""
