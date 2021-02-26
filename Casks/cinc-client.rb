6b52b9f137bb78511d3b7ba4386e167fad4a5035
Don Lewis <don.wayne.lewis@gmail.com>
2021-02-26 09:03:20 -0700

    Changing the arch a little.

--- Casks/cinc-client.rb
+++ Casks/cinc-client.rb
@@ -1,15 +1,16 @@
 cask "cinc-client" do
   version "16.10.17"
   sha256 "b41b31098eaefdefc0bcdd26f6c7da1b1d9314c2cf0b9a68ef23895c67bcb76c"
+  arch "x86_64"
 
-  url "http://downloads.cinc.sh/files/stable/cinc/#{version}/mac_os_x/10.14/cinc-#{version}-1.x86_64.dmg"
+  url "http://downloads.cinc.sh/files/stable/cinc/#{version}/mac_os_x/10.14/cinc-#{version}-1.#{arch}.dmg"
   appcast ""
   name "CINC Auditor"
   homepage "https://cinc.sh/start/client/"
 
   depends_on macos: ">= :high_sierra"
 
-  pkg "cinc-#{version}-1.pkg"
+  pkg "cinc-#{version}-1.#{arch}.pkg"
 
   uninstall_postflight do
     system_command "/usr/bin/find",
