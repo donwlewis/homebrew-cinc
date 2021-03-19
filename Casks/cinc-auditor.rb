ac727536c0a2e162be08d11a247374470a24c505
Don Lewis <don.wayne.lewis@gmail.com>
2021-03-19 09:01:58 -0600

    Updated cinc-auditor to version 4.28.0

--- Casks/cinc-auditor.rb
+++ Casks/cinc-auditor.rb
@@ -1,5 +1,5 @@
 cask "cinc-auditor" do
-  version "4.26.13"
+  version "4.28.0"
   sha256 "a2e7c550c6d631030ad032283fd1153c134eafcf74539011bc3b7f95a3021338"
 
   url "http://downloads.cinc.sh/files/stable/cinc-auditor/#{version}/mac_os_x/10.14/cinc-auditor-#{version}-1.x86_64.dmg"
